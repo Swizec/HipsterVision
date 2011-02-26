0131347b3f7db923ec50439659026088df8b8be0
Swizec Teller <swizec@swizec.com>
2011-02-26 22:03:58 +0100

    node.js api changes fast

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 var http = require('http'),
-redis = require('redis').createClient();
+redis = require('redis').createClient(),
+EventEmitter = require('events').EventEmitter;
 
 var timeouter = new EventEmitter();
 
@@ -18,7 +19,6 @@ var server = http.createServer(function (req, res) {
     redis.on("message", push_message);
     timeouter.on("timeout-poll", function () {
 	res.writeHead(200);
-	res.write("timeout prevention");
 	res.end();
     })
 });
