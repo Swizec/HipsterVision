b8affec73e78f1fced566fbb00ec65b2a1e8f14b
Swizec Teller <swizec@swizec.com>
2011-02-26 23:03:32 +0100

    this should display images when long poll comes in

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -6,7 +6,18 @@ $(document).ready(function () {
     var long_poll = function () {
 	window.log("polling ...");
 	$.get('/publish/', function (data) {
-	    $("#main").append("<div>"+data+"</div>");
+	    var data = $.parseJSON(data);
+	    
+	    for (var i = 0; i<data.length; i++) {
+		$.get("https://api.instagram.com/v1/media/search?lat=37.7793&lng=-122.4192&client_id=d1ca75d66977495db80ff240d54eb6d4&distance=5000&max_timestamp="+data[i]['time']+"&min_timestamp="+(data[i]['time']-50),
+		      function (data) {
+			  var images = $.parseJSON(data);
+			  var image = images[images.length-i-1];
+			  
+			  $("#main").append('<img src="'+image['data']['images']['low_resolution']['url']+'" />');
+		      });
+	    }
+
 	    long_poll();
 	});
     }
