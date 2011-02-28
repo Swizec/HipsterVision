fa9144cc77c8bc83c2ec970ef251d7ae98c2ecb0
Swizec Teller <swizec@swizec.com>
2011-02-28 16:37:41 +0100

    debugging stuff

--- listener.js
+++ listener.js
@@ -71,6 +71,8 @@ var publish_images = function (input_data) {
 		console.log("got images");
 		var images = JSON.parse(data);
 
+		console.log(images);
+
 		var published = false;
 		for (var j = 0; j < images.length; j++) {
 		    var image = images[j];
