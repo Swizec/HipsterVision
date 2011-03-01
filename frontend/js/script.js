3a332e462a0aaf408019669a3c3b454b0318f6f3
Swizec Teller <swizec@swizec.com>
2011-03-01 22:11:18 +0100

    frontend interface that performs reverse geocoding

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -2,6 +2,20 @@
 
 */
 
+// from 
+// http://www.bloggingdeveloper.com/post/JavaScript-QueryString-ParseGet-QueryString-with-Client-Side-JavaScript.aspx
+function getQuerystring(key, default_)
+{
+  if (default_==null) default_=""; 
+  key = key.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+  var regex = new RegExp("[\\?&]"+key+"=([^&#]*)");
+  var qs = regex.exec(window.location.href);
+  if(qs == null)
+    return default_;
+  else
+    return qs[1];
+}
+
 $(document).ready(function () {
     var long_poll = function () {
 	window.log("polling ...");
@@ -15,13 +29,30 @@ $(document).ready(function () {
 	});
     }
 
-    long_poll();
+    //long_poll();
+
+    var query = getQuerystring('search', '');
+
+    if (query != '') {
+	find_pics(query);
+    }
 });
 
 
+function find_pics(query) {
+    var geocoder = new google.maps.Geocoder();
 
-
-
+    geocoder.geocode({address: query},
+		     function (result, status) {
+			 if (status == 'OK') {
+			     $("#search input[type='text']").val(result[0].formatted_address);
+			     
+			     alert(result[0].geometry.location);
+			 }else{
+			     alert('Fuck! Something went wrong talking to google');
+			 }
+		     });
+}
 
 
 
