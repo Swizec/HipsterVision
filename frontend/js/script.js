69f0769911ea6e8035bb8c6c974b11e0f3031f16
Swizec Teller <swizec@swizec.com>
2011-03-02 01:44:20 +0100

    indentation love

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -53,51 +53,51 @@ function find_pics(query) {
 		     });
 }
 
-// random find on internets
+// random find on internets and fix'd a bit
 function isodatetime(today) {
-var today = today || new Date();
-var year = today.getYear();
-if (year < 2000) // Y2K Fix, Isaac Powell
-year = year + 1900; // http://onyx.idbsu.edu/~ipowell
-var month = today.getMonth() + 1;
-var day = today.getDate();
-var hour = today.getHours();
-var hourUTC = today.getUTCHours();
-var diff = hour - hourUTC;
-var hourdifference = Math.abs(diff);
-var minute = today.getMinutes();
-var minuteUTC = today.getUTCMinutes();
-var minutedifference;
-var second = today.getSeconds();
-var timezone;
-if (minute != minuteUTC && minuteUTC < 30 && diff < 0) { hourdifference--; }
-if (minute != minuteUTC && minuteUTC > 30 && diff > 0) { hourdifference--; }
-if (minute != minuteUTC) {
-minutedifference = ":30";
-}
-else {
-minutedifference = ":00";
-}
-if (hourdifference < 10) { 
-timezone = "0" + hourdifference + minutedifference;
-}
-else {
-timezone = "" + hourdifference + minutedifference;
-}
-if (diff < 0) {
-timezone = "-" + timezone;
-}
-else {
-timezone = "+" + timezone;
-}
-if (month <= 9) month = "0" + month;
-if (day <= 9) day = "0" + day;
-if (hour <= 9) hour = "0" + hour;
-if (minute <= 9) minute = "0" + minute;
-if (second <= 9) second = "0" + second;
-time = year + "-" + month + "-" + day + "T"
-+ hour + ":" + minute + ":" + second;
-return time;
+    var today = today || new Date();
+    var year = today.getYear();
+    if (year < 2000) // Y2K Fix, Isaac Powell
+	year = year + 1900; // http://onyx.idbsu.edu/~ipowell
+    var month = today.getMonth() + 1;
+    var day = today.getDate();
+    var hour = today.getHours();
+    var hourUTC = today.getUTCHours();
+    var diff = hour - hourUTC;
+    var hourdifference = Math.abs(diff);
+    var minute = today.getMinutes();
+    var minuteUTC = today.getUTCMinutes();
+    var minutedifference;
+    var second = today.getSeconds();
+    var timezone;
+    if (minute != minuteUTC && minuteUTC < 30 && diff < 0) { hourdifference--; }
+    if (minute != minuteUTC && minuteUTC > 30 && diff > 0) { hourdifference--; }
+    if (minute != minuteUTC) {
+	minutedifference = ":30";
+    }
+    else {
+	minutedifference = ":00";
+    }
+    if (hourdifference < 10) { 
+	timezone = "0" + hourdifference + minutedifference;
+    }
+    else {
+	timezone = "" + hourdifference + minutedifference;
+    }
+    if (diff < 0) {
+	timezone = "-" + timezone;
+    }
+    else {
+	timezone = "+" + timezone;
+    }
+    if (month <= 9) month = "0" + month;
+    if (day <= 9) day = "0" + day;
+    if (hour <= 9) hour = "0" + hour;
+    if (minute <= 9) minute = "0" + minute;
+    if (second <= 9) second = "0" + second;
+    time = year + "-" + month + "-" + day + "T"
+	+ hour + ":" + minute + ":" + second;
+    return time;
 }
 
 function display_images(images) {
@@ -111,7 +111,7 @@ function display_images(images) {
     for (var i = 0; i < images.length; i++) {
 	var d = new Date();
 	d.setTime(images[i].created_time*1000);
-	$proto.clone().attr('class', 'image').appendTo($target).find('img').attr('src', images[i].images.low_resolution.url).siblings('label').html('<strong>'+images[i].user.username+'</strong> <time datetime="'+(isodatetime(d))+'" class="timeago"></time>'+((images[i].caption != null) ? '<p>'+images[i].caption.text+'</p>' : ''));
+	$proto.clone().attr('class', 'image').appendTo($target).find('img').attr('src', images[i].images.low_resolution.url).siblings('label').html('<strong>'+images[i].user.username+'</strong> <time datetime="'+(isodatetime(d))+'" class="timeago"></time>'+((images[i].caption != null) ? '<br/>'+images[i].caption.text : ''));
     }
 
     $('time').timeago();
