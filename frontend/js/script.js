740102b97a6310a889490f908d5a79d1f00eba1a
Swizec Teller <swizec@swizec.com>
2011-03-04 02:52:30 +0100

    displaying tag suggestions well

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -127,9 +127,9 @@ function display_images(images) {
     }
     if (typeof(images.tags) != 'undefined') {
 	pagination_data.tags = images.tags;
-	$tags = $(".tags").html('Try: ');
-	for (var i=0; i<images.tags.length; i++) {
-	    $tags.append('<a href="?search=%23'+images.tags[i].name+'>'+images.tags[i].name+'</a> ');
+	var $tags = $(".tags").html('Try: ');
+	for (var i=1; i<images.tags.length && i < 10; i++) {
+	   $tags.append('<a href="?search=%23'+images.tags[i].name+'">#'+images.tags[i].name+'</a>');
 	}
     }
 
