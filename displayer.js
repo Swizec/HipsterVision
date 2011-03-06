6c5f499a0610e30472ac6592962216bb1f809943
Swizec Teller <swizec@swizec.com>
2011-03-06 02:30:13 +0100

    displayer knows how to fetch specific image

--- displayer.js
+++ displayer.js
@@ -2,23 +2,14 @@
 var http = require('http'),
     fs = require('fs'),
     redis = require('redis').createClient(),
+    settings = require('./settings.js'),
+    instagram = require('instagram').createClient(settings.client_id, settings.client_secret),
     parrot = require('parrot');
 
 
 var server = http.createServer(function (req, res) {
-    res.writeHead(200);
-
-    redis.get('HV:last-search', function (err, data) {
-	if (data != null) {
-	    var data = JSON.parse(data);
-	    var images = data['result']['images'];
-	    var search_query = data['query'];
-	}else{
-	    var images = [];
-	    var search_query = '';
-	}
-	//console.log(images);
-	
+    var serve = function (images, search_query) {
+	res.writeHead(200);
 	fs.readFile('frontend/index.html', function(err, data) {
 	    res.write(parrot.render(data,
 				    {cache: 0,
@@ -26,7 +17,34 @@ var server = http.createServer(function (req, res) {
 					       search_query: search_query}}));
 	    res.end();
 	});
-    });
+    }
+
+    if (req.url.substring(0, 5) == '/pic/') {
+	var id = req.url.split('/')[2];
+	redis.get('HV:image:'+id, function (err, data) {
+	    if (data == null) {
+		instagram.media.id(id, function (image, error) {
+		    redis.set('HV:image:'+id, JSON.stringify(image));
+		    serve([image], '');
+		});
+	    }else{
+		serve([JSON.parse(data)], '');
+	    }
+	});
+    }else{
+	redis.get('HV:last-search', function (err, data) {
+	    var images = [];
+	    var search_query = '';
+
+	    if (data != null) {
+		var data = JSON.parse(data);
+		images = data['result']['images'];
+		search_query = data['query'];
+	    }
+
+	    serve(images, search_query);
+	});
+    }
 });
 
 var port = process.argv[2];
